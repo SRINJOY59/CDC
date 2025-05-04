0c30a4accbf8e6088bfc7f10bba8be2b783cc68c
SRINJOY59 <srinjoydas566@gmail.com>
2025-05-05 05:10:55 +0530

    deque done

--- Atcoder/Educational_DP/L-Deque.cpp
+++ Atcoder/Educational_DP/L-Deque.cpp
@@ -1,33 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
 
-ll solve(ll start, ll end, vector<ll>&stones, int flag, vector<vector<vector<ll>>>&dp){
-    if(start > end){
-        return 0;
+pair<ll, ll> solve(vector<ll>& a, ll start, ll end, vector<vector<pair<ll, ll>>>& dp) {
+    if (start == end) {
+        return {a[start], 0};
     }
-    if(dp[start][end][flag] != -1){
-        return dp[start][end][flag];
+    if (dp[start][end].first != -1) {
+        return dp[start][end];
     }
 
-    if(flag == 0){
-        return dp[start][end][flag] = max(stones[start] + solve(start+1, end, stones, 1, dp), stones[end] + solve(start, end-1, stones, 1, dp));
+    auto opt1 = solve(a, start + 1, end, dp);
+    ll score1 = a[start] + opt1.second;
+    
+    auto opt2 = solve(a, start, end - 1, dp);
+    ll score2 = a[end] + opt2.second;
+
+    if (score1 > score2) {
+        dp[start][end] = {score1, opt1.first};
+    } else {
+        dp[start][end] = {score2, opt2.first};
     }
-    else{
-        return dp[start][end][flag] = min(-stones[start] + solve(start+1, end, stones, 0, dp), -stones[end] + solve(start, end-1, stones, 0, dp));
-    }
+
+    return dp[start][end];
 }
 
-
-int main(){
+signed main() {
     ll n;
-    cin>>n;
-    vector<ll>stones(n);
-    for(ll i = 0;i<n;i++){
-        cin>>stones[i];
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) {
+        cin >> a[i];
     }
-    vector<vector<vector<ll>>>dp(n, vector<vector<ll>>(n, vector<ll>(2, -1)));
-    cout<<solve(0, n-1, stones, 0, dp)<<endl;
-    return 0;  
+
+    // pair<ll, ll> -> first element is current players score, second element is opponent's score
+
+    vector<vector<pair<ll, ll>>> dp(n, vector<pair<ll, ll>>(n, {-1, -1}));
+    auto res = solve(a, 0, n - 1, dp);
+    cout << res.first - res.second << "\n";
+
+    return 0;
 }
